9dbfddbf1387b3c61a153d38c5570f1516568438
1mDanil <danya7540@gmail.com>
2024-12-05 20:37:43 +0300

    Add files via upload

--- Module_5_1.py
+++ Module_5_1.py
@@ -18,6 +18,38 @@ class House:
     def __str__(self):
         return f'{self.name}, кол-во этажей {self.number_of_floors}'
 
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __iadd__(self, value):
+        self.number_of_floors += value
+        return self
+
+
+
 
 
 house_1 = House('ЖК Горский', 18)
@@ -26,7 +58,23 @@ house_2 = House('Домик в деревне', 2)
 #house_1.go_to(5)
 #house_2.go_to(10)
 
-print(house_1)
+#print(house_1)    домашняя работа 2
+#print(house_2)
+#print(len(house_1))
+#print(len(house_2))
+
+print(house_1)              #домашняя работа 3
 print(house_2)
-print(len(house_1))
-print(len(house_2))
+print(house_1 == house_2)
+house_1 = house_1 + 10
+print(house_1)
+print(house_1 == house_2)
+house_2 = 10 + house_2
+print(house_2)
+house_1 += 10
+print(house_1)
+print(house_1 > house_2)
+print(house_1 >= house_2)
+print(house_1 < house_2)
+print(house_1 <= house_2)
+print(house_1 != house_2)
